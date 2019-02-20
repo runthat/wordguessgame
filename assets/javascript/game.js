54692e993fa35a54adead2f062f6b445886db8ac
unknown <mdknightslifetell@gmail.com>
2019-02-20 04:45:24 -0700

    first commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,114 @@
 
+//variables
+
+var words = ["audi", "honda", "acura", "chrysler", "dodge", "buick", "ford", "lincoln", "tesla", "toyota"];
+var randomWord = "";
+var lettersOfWord = []
+var blanks = 0;
+var blanksAndCorrect = [];
+var wrongGuess = [];
+var wins = 0;
+var losses = 0;
+var guessesRemaining = 9;
+var carImage = "";
+
+
+//game start
+function Game() {
+    
+    
+    randomWord = words[Math.floor(Math.random() * words.length)];
+    carImage = "assets/images/" + randomWord + ".jpg";
+
+    lettersOfWord = randomWord.split("");
+
+    blanks = lettersOfWord.length;
+
+    for (var i = 0; i < blanks; i++) {
+        blanksAndCorrect.push("_");
+    }
+
+    document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join("  ");
+    
+    //display car
+    document.getElementById("image").src = carImage;
+    console.log(document.getElementById("image"));
+    
+    //console logging 
+
+    console.log(randomWord);
+    console.log(lettersOfWord);
+    console.log(blanks);
+    console.log(blanksAndCorrect);
+    console.log(carImage);
+};
+
+
+//reset function
+function reset() {
+    guessesRemaining = 9;
+    wrongGuess = [];
+    blanksAndCorrect = [];
+    Game()
+}
+
+//check letters
+function checkLetters(letter) {
+    var letterInWord = false;
+    for (var i = 0; i < blanks; i++) {
+        if (randomWord[i] == letter) {
+            letterInWord = true;
+        }
+    }
+    if (letterInWord) {
+        for (var i = 0; i < blanks; i++) {
+            if (randomWord[i] == letter) {
+                blanksAndCorrect[i] = letter;
+            }
+        }
+    }
+    else {
+        wrongGuess.push(letter);
+        guessesRemaining--;
+    }
+    console.log(blanksAndCorrect);
+}
+
+//completion function
+
+function complete() {
+    console.log("wins:" + wins + "| losses:" + losses + "| guesses left:" + guessesRemaining)
+
+    if (lettersOfWord.toString() == blanksAndCorrect.toString()) {
+        wins++;
+        //aud()
+        reset()
+
+        document.getElementById("winstracker").innerHTML = " " + wins;
+
+    } else if (guessesRemaining === 0) {
+        losses++;
+        reset()
+        document.getElementById("image").src = "./assets/images/try-again.png"
+        reset()
+        document.getElementById("losstracker").innerHTML = " " + losses;
+    }
+
+    document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join(" ");
+    document.getElementById("guessesremaining").innerHTML = " " + guessesRemaining;
+}
+
+
+//execute
+
+Game()
+
+document.onkeyup = function (event) {
+    var guesses = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetters(guesses);
+    complete();
+    console.log(guesses);
+
+    document.getElementById("playerguesses").innerHTML = "  " + wrongGuess.join(" ");
+}
+
